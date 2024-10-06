756fe91b334d6739dd90bbeef86dca6bee4835d7
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-06 09:30:53 +0200

    Set up body format and basic css styling

--- Meme-Gen/src/app.jsx
+++ Meme-Gen/src/app.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../componenets/header";
+import Meme from "../../componenets/Meme";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
   );
 }
--- componenets/header.jsx
+++ componenets/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" />
+      <img src="./troll-face.png" />
       <h2>Header component</h2>
       <h4>React Course - Project 3</h4>
     </header>

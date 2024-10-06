880d6736d1d6d1d37e6d90ad51d4c263beed26bc
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-06 20:49:23 +0200

    Fixed troll face render issue

--- Meme-Gen/src/app.jsx
+++ Meme-Gen/src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../../componenets/header";
-import Meme from "../../componenets/Meme";
+import Header from "./componenets/header";
+import Meme from "./componenets/Meme";
 
 export default function App() {
   return (
--- Meme-Gen/src/componenets/header.jsx
+++ Meme-Gen/src/componenets/header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import trollFace from "../images/trollFace.png";
 export default function Header() {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" className="header--image" />
+      <img src={trollFace} className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
